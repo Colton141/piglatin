43c06fb4f368420763f86a6bcd524dcfc5fdc500
dominic montelongo and Nathan Aden <nathan@>
2019-04-03 14:47:22 -0700

    add a branch in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,20 @@ function pigLatinAWord (word) {
   if (isAVowel(word[0]) === true) return word + "way"
 
 
+  for (var i=0; i<word.length;i = i +1) {
+    if (isAVowel(word[i]) === true) {
+      if (i > 0 && word[i] === "u" && word[i - 1] === "q") {
+        return word.slice(i + 1) + word.slice(0, i + 1) + "ay"
+      } else {
+        return word.slice(i) + word.slice(0, i) + "ay"
+      }
+    }
+  }
 
 
-  return word;
+
+
+return word;
 }
 
 
